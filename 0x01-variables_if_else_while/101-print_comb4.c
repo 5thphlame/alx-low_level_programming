8c80c068045f4fd7d327cf48120fba184a6be923
5thphlame <princemcamah@gmail.com>
2022-03-12 05:21:09 -0800

    comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,16 +11,16 @@ int main(void)
 	int ci; /*count decimas*/
 	int ciii;
 
-	c= 48;
+	c = 48;
 	ci = 48;
 	ciii = 48;
 	for (ci = 48; ci < 58; ci++)
 	{
 		for (c = 48; c < 58; c++)
 		{
-			for(ciii = 48; ciii < 58; ciii++)
+			for (ciii = 48; ciii < 58; ciii++)
 			{
-				if((c > ci) && (ciii > c))
+				if ((c > ci) && (ciii > c))
 				{
 					putchar(ci);
 					putchar(c);
