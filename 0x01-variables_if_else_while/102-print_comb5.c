3eb1d14eb4f20c30d479f40f6e516ad2544433e2
5thphlame <princemcamah@gmail.com>
2022-03-12 05:56:02 -0800

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,9 +10,9 @@
 int main(void)
 {
 	int num1, num2;
-	for (num1 = 0; num1 <= 98; num1++)
+	for (num1 = 0; num1 <= 98; num1++;)
 	{
-		for  (num2 = num1 <= 99; num2++)
+		for (num2 = num1 <= 99; num2++;)
 		{
 			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
