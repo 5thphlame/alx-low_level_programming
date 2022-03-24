a8a73405b8d6c994d0eae4e4ca99ce89740a0568
5thphlame <princemcamah@gmail.com>
2022-03-24 02:57:26 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -11,3 +14,5 @@ void print_number(int n);
 int _putchar(char c);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+
+#endif
