b273221b9c96118da99c6708c1da3557588683e0
5thphlame <princemcamah@gmail.com>
2022-03-23 19:14:18 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,30 +8,16 @@
  * @src: pointer to source string.
  * @n: number of bytes to be used.
  *
- *         *
- *
- *          * Return: pointer to destination string.
- *
- *           */
-
+ * Return: pointer to destination string.
+ */
 char *_strncpy(char *dest, char *src, int n)
-
 {
-
 	int byteCount;
 
-
-
 	for (byteCount = 0; byteCount < n && src[byteCount] != '\0'; byteCount++)
-
 		dest[byteCount] = src[byteCount];
-
 	for (; byteCount < n; byteCount++)
-
 		dest[byteCount] = '\0';
 
-
-
 	return (dest);
-
 }
