ce549051019c02c386c1eb1570a55cea967b1741
Prince Amah <princemcamah@gmail.com>
2022-04-01 21:35:42 +0100

    update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-#define UNUSED(x) (void)(x)
 /**
- * main - prints out program name
- * @argc: number of args to be passed
- * @argv: strings passed
- * Return: alway 0
- *
+ * main - prints its name, followed by a new line.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	UNUSED(argc);
 	printf("%s\n", argv[0]);
 	return (0);
 }
