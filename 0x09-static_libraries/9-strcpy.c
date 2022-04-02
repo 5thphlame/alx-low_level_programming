2a47551c2b101348e7dabe0a815e698d1d854e84
Prince Amah <princemcamah@gmail.com>
2022-04-02 04:21:02 +0100

    update

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _strcpy - Copy paste string
- * @dest: destination
- * @src: source
- * Return: dest
- */
+* _strcpy - Copy paste string
+*@dest: destination
+*@src: source
+*Return: dest
+*/
 char *_strcpy(char *dest, char *src)
 {
 int inc = 0;
