caa85711f8c6da76b01135dd2d07396f666d7015
5thphlame <87295779+5thphlame@users.noreply.github.com>
2022-04-03 17:24:21 +0100

    Update

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,20 +1,11 @@
 #include "main.h"
-/**
-*_memset - The _memset() function fills
-* the first n bytes of the memory area
-* pointed to by s with the constant byte b
-*@s:target
-*@b: constant byte
-*@n:number of byte
-*Return: returns new value of target
-*/
 
-char *_memset(char *s, char b, unsigned int n)
+/**
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
 {
-	while (n)
-	{
-		s[n - 1] = b;
-		n--;
-	}
-	return (s);
+	return (c >= 'A' && c <= 'Z');
 }
