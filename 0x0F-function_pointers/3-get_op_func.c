ee3ede1f2b772d22a08f2f8b45ab53637a1dd662
5thphlame <87295779+5thphlame@users.noreply.github.com>
2022-04-13 21:02:50 +0100

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,48 +1,29 @@
-#include "3-calc.h"
 #include <stdlib.h>
-#include <string.h>
-
+#include "3-calc.h"
 /**
-<<<<<<< HEAD
- * get_op_func - ...
- * @s: ...
+ * get_op_func - get funtion
  *
- * Return: ...
+ * @s: pointer to funtion
+ *
+ * Return: Integer
  */
-=======
-  * get_op_func - ...
-  * @s: ...
-  *
-  * Return: ...
-  */
->>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-	{ "+", op_add },
-	{ "-", op_sub },
-	{ "*", op_mul },
-<<<<<<< HEAD
-	{ "/", op_div }
-=======
-	{ "/", op_div },
->>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
-	{ "%", op_mod },
-	{ NULL, NULL }
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL},
 	};
 	int i = 0;
 
 	while (i < 5)
 	{
-		if (strcmp(s, ops[i].op) == 0)
+		if (*(ops[i]).op == *s)
 			return (ops[i].f);
-<<<<<<< HEAD
-		
-=======
-
->>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
 		i++;
 	}
-
-	return (0);
+	return (NULL);
 }
