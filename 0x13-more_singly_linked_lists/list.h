f92ea228fd1f6c519a07dc613a025c95aaebfad9
5thphlame <87295779+5thphlame@users.noreply.github.com>
2022-04-26 19:22:28 +0100

    List Header

--- 0x13-more_singly_linked_lists/list.h
+++ 0x13-more_singly_linked_lists/list.h
@@ -1,17 +1,16 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *
+ * for ALX project
  */
 typedef struct listint_s
 {
@@ -32,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
-size_t free_listint_safe(listint_t **head);
+size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
 #endif
